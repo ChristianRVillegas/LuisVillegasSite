ddad63dd81bd3451bbec62ccb64c264e1f8050ec
ChristianRVillegas <ChristianRVillegas2700@gmail.com>
2023-04-08 14:14:59 -0700

    created react project and basic hompage

---
+++ portfolio-site/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to my portfolio website!</h1>
+            <p>Here you will find information about my father's professional music career.</p>
+        </div>
+    );
+}
+
+
+
+
+
+export default HomePage;
